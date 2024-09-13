f42e94b733fcdc21990299fff139ee96d54f0c24
Sefa Kahraman <devsefakahraman@gmail.com>
2024-09-14 00:45:56 +0300

    Add project files.

---
+++ OOP/Kitap.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace OOP
+{
+    public class Kitap
+    {
+        private string yazarSoyadi;
+
+        public Kitap()
+        {
+            Console.WriteLine("\n**KİTAP OLUŞTURULDU**");    
+        }
+
+        public string KitapAdi { get; set;}
+        public string SayfaSayisi { get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi
+        {
+            get
+            {
+                return yazarSoyadi;
+            }
+            set
+            {
+                yazarSoyadi = value.ToUpper();
+
+            }
+
+        }
+        public string YazarinTamAdi => $"{YazarAdi} {YazarSoyadi}"; //lambda sadece get
+
+        public void KitapYazdirBaskaMetodla()
+        {
+            Console.WriteLine($"" +
+                $"\nKitap Adı: {KitapAdi}" +
+                $"\nSayfa Sayısı: {SayfaSayisi}" +
+                $"\nYazar Adı: {YazarAdi}" +
+                $"\nYazar Soyadı: {YazarSoyadi}" +
+                $"\nYazarın Tam Adı: {YazarinTamAdi}");
+        }
+
+        public override string ToString()
+        {
+            return $"" +
+                $"\nKitap Adı: {KitapAdi}" +
+                $"\nSayfa Sayısı: {SayfaSayisi}" +
+                $"\nYazar Adı: {YazarAdi}" +
+                $"\nYazar Soyadı: {YazarSoyadi}" +
+                $"\nYazarın Tam Adı: {YazarinTamAdi}";
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using OOP;
+
+Kitap kitap1 = new()
+{
+    KitapAdi ="Ihlamurlar Altında",
+    SayfaSayisi = "568",
+    YazarAdi ="Sefa",
+    YazarSoyadi="Kahraman",
+};
+
+Console.WriteLine(kitap1.ToString());
+
+
+Kitap kitap2 = new Kitap() { KitapAdi = "Ver Elini", SayfaSayisi = "789", YazarAdi = "Ahmet", YazarSoyadi = "Hamdi" };
+
+kitap2.KitapYazdirBaskaMetodla();
+
+Kitap kitap3 = new Kitap();
+kitap3.KitapAdi = "Aynı Yıldız";
+kitap3.SayfaSayisi = "421";
+kitap3.YazarAdi = "Mehmet";
+kitap3.YazarSoyadi = "Memlük";
+
+kitap3.KitapYazdirBaskaMetodla();
